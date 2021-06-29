dff765018edf8739dfe1643153aacd303877b865
leotan100 <86002224+leotan100@users.noreply.github.com>
2021-06-29 09:35:21 -0700

    random message

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -4,7 +4,7 @@ import useKeyPress from "../../hooks/use-key-press";
 import useWalk from "../../hooks/use-walk"
 
 export default function Player({ skin }) {
-  const { dir, step, walk } = useWalk(3)
+  const { dir, step, walk, position } = useWalk(3)
   const data = {
      h: 32,
      w: 32,
@@ -13,11 +13,19 @@ export default function Player({ skin }) {
    
 
    useKeyPress((e) => {
-     const dir = e.key.replace("Arrow", "").toLowerCase()
-     walk(dir)
+     walk(e.key.replace("Arrow", "").toLowerCase());
 
      e.preventDefault();
    });
 
-  return <Actor sprite={`/Sprite/skins/${skin}.png`} data={data} />;
-}
+
+  return ( 
+     <Actor 
+  sprite={`/Sprite/skins/${skin}.png`} 
+  data={data}
+  step={step} 
+  dir={dir}
+  position={position}
+   />
+  )
+  };
